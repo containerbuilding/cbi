3501ba5a9e0fb587408b4654e4a144d494fc89d1
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2018-05-30 22:40:41 +0900

    dind: update
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

--- hack/dind/up.sh
+++ hack/dind/up.sh
@@ -10,26 +10,11 @@ set -x
 cd $(dirname $0)/../..
 source ./hack/dind/config
 
-# Download Mirantis/kubeadm-dind-cluster 
+# Download kubeadm-dind-cluster
 if [ ! -x ${DIND_CLUSTER_SH} ]; then
     wget -O ${DIND_CLUSTER_SH} ${DIND_CLUSTER_SH_URL}
     chmod +x ${DIND_CLUSTER_SH}
 fi
-
-# <WORKAROUND>
-# https://github.com/Mirantis/kubeadm-dind-cluster/pull/128
-img=mirantis/kubeadm-dind-cluster:v1.9
-docker pull ${img}
-tmp=$(mktemp -d)
-cid=$(docker create ${img})
-docker cp ${cid}:/usr/local/bin/rundocker ${tmp}
-patch -d ${tmp} -p2 < ./hack/dind/image-rundocker.patch
-docker cp ${tmp}/rundocker ${cid}:/usr/local/bin/rundocker
-docker commit ${cid} ${img}
-docker rm ${cid}
-rm -rf ${tmp}
-export DIND_SKIP_PULL=1
-# </WORKAROUND>
 
 # Start kube
 DIND_INSECURE_REGISTRIES="[\"${CBI_REGISTRY}:5000\"]" DIND_DAEMON_JSON_FILE=/dev/null ${DIND_CLUSTER_SH} up
